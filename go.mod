4e74a41ad8428a1c755b2160f36a40e38021c40a
zjyl1994 <me@zjyl1994.com>
2020-04-30 13:50:23 +0800

    add gin server

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/zjyl1994/livetv
 
 go 1.13
 
-require github.com/go-ini/ini v1.55.0
+require (
+	github.com/gin-gonic/gin v1.6.2
+	github.com/go-ini/ini v1.55.0
+)
--- main.go
+++ main.go
@@ -1,8 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	initProc()
-	fmt.Println(cfg)
+	err := initProc()
+	if err != nil {
+		log.Fatalf("init: %s\n", err)
+	}
+	log.Println("LiveTV starting...")
+	router := gin.Default()
+	router.GET("/", func(c *gin.Context) {
+		time.Sleep(5 * time.Second)
+		c.String(http.StatusOK, "Welcome Gin Server")
+	})
+	srv := &http.Server{
+		Addr:    cfg.ListenOn,
+		Handler: router,
+	}
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+	quit := make(chan os.Signal)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("Shuting down server...")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server forced to shutdown:", err)
+	}
+	log.Println("Server exiting")
 }
