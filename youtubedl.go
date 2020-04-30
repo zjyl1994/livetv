2ce538857b34993e9b8674e440d342ace7fea413
zjyl1994 <me@zjyl1994.com>
2020-04-30 13:43:20 +0800

    youtube-dl integrated

---
+++ youtubedl.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"context"
+	"os/exec"
+	"strings"
+	"time"
+)
+
+func getYoutubeLiveM3U8(youtubeURL string) (string, error) {
+	ytdlArgs := strings.Fields(cfg.YtdlArgs)
+	for i, v := range ytdlArgs {
+		if strings.EqualFold(v, "{url}") {
+			ytdlArgs[i] = youtubeURL
+		}
+	}
+	_, err := exec.LookPath(cfg.YtdlCmd)
+	if err != nil {
+		return "", err
+	} else {
+		ctx, cancelFunc := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancelFunc()
+		cmd := exec.CommandContext(ctx, cfg.YtdlCmd, ytdlArgs...)
+		out, err := cmd.CombinedOutput()
+		return string(out), err
+	}
+}
